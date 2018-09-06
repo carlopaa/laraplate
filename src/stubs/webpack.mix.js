fbb7ce7d53fb84b9f084e38baeb7f1d48fc96ba6
Carlo Garcia Paa <codingprogress@outlook.com>
2018-09-06 11:18:10 +0800

    New laravel version

--- src/stubs/webpack.mix.js
+++ src/stubs/webpack.mix.js
@@ -1,17 +1,17 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/assets/js/app.js', 'js')
-    .sass('resources/assets/sass/app.scss', 'css')
+mix.js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css')
     .sourceMaps()
     .webpackConfig({
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'resources'),
-                'styles': path.resolve(__dirname, 'resources/assets/sass'),
-                'images': path.resolve(__dirname, 'resources/assets/images'),
-                'fonts': path.resolve(__dirname, 'resources/assets/fonts'),
-                'scripts': path.resolve(__dirname, 'resources/assets/js'),
-                'components': path.resolve(__dirname, 'resources/assets/js/components')
+                'styles': path.resolve(__dirname, 'resources/sass'),
+                'images': path.resolve(__dirname, 'resources/images'),
+                'fonts': path.resolve(__dirname, 'resources/fonts'),
+                'scripts': path.resolve(__dirname, 'resources/js'),
+                'components': path.resolve(__dirname, 'resources/js/components')
             }
         }
     }).version();
